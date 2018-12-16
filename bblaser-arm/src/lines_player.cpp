b0e02d66174592c7b89672b9b7c907cf90936474
gijs.sijpesteijn <gijs@jpoint.nl>
2018-12-16 20:04:26 +0100

    meer

--- bblaser-arm/src/lines_player.cpp
+++ bblaser-arm/src/lines_player.cpp
@@ -59,8 +59,8 @@ void lines_player::stop() {
     this->lp->setRed(0);
     this->lp->setGreen(0);
     this->lp->setBlue(0);
-    point p(0,0);
-    this->lp->setPoint(&p);
+//    point p(0,0);
+//    this->lp->setPoint(&p);
     log::debug("Stopping");
     if (pthread_mutex_unlock(&player_lock) != 0) {
         log::error("Lines player: Can't unlock on the player state.");
